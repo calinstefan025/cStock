a3d7549481d87bc3ad8fe22bc94abe8a72f8e369
calinstefan025 <clavramoniu@gmail.com>
2022-07-30 22:12:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+/////////////////////////////////////////
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+/////////////////////////////////////////
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
