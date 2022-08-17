d58efe525d5e51312ca7d41d392e8f0aefd013bc
Avramoniu Calin <clavramoniu@gmail.com>
2022-08-17 23:14:33 +0300

    stock page update

--- routers/stock.js
+++ routers/stock.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const bodyParser = require("body-parser");
 var request = require("request");
 
+const api_key = "3XWA7YP44XQ5VGAH";
+
 router.get("/:ticker/overview", (req, res) => {
   const urlTicker = req.params.ticker;
-  var urlOverview =
+  const urlOverview =
     "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
     urlTicker +
-    "&apikey=3XWA7YP44XQ5VGAH";
-
+    "&apikey=" +api_key;
   request.get(
     {
       url: urlOverview,
@@ -22,25 +23,51 @@ router.get("/:ticker/overview", (req, res) => {
       } else {
         // res.send(data);
         console.log(data);
-        res.render("stock", {
+        const object = {
           name: data.Name,
           description: data.Description,
-        });
+          MarketCapitalization: data.MarketCapitalization,
+          EBITDA: data.EBITDA,
+          PERatio: data.PERatio,
+          PEGRatio: data.PEGRatio,
+          BookValue: data.BookValue,
+          DividendPerShare: data.DividendPerShare,
+          DividendYield: data.DividendYield,
+          EPS: data.EPS,
+          RevenuePerShareTTM: data.RevenuePerShareTTM,
+          ProfitMargin: data.ProfitMargin,
+          OperatingMarginTTM: data.OperatingMarginTTM,
+          ReturnOnAssetsTTM: data.ReturnOnAssetsTTM,
+          ReturnOnEquityTTM:data.ReturnOnEquityTTM,
+          RevenueTTM: data.RevenueTTM,
+          GrossProfitTTM: data.GrossProfitTTM,
+          DilutedEPSTTM: data.DilutedEPSTTM,
+          QuarterlyEarningsGrowthYOY: data.QuarterlyEarningsGrowthYOY,
+          QuarterlyRevenueGrowthYOY: data.QuarterlyRevenueGrowthYOY,
+          AnalystTargetPrice: data.AnalystTargetPrice,
+          TrailingPE: data.TrailingPE,
+          ForwardPE: data.ForwardPE,
+          PriceToSalesRatioTTM: data.PriceToSalesRatioTTM,
+          PriceToBookRatio: data.PriceToBookRatio,
+          EVToRevenue: data.EVToRevenue,
+          EVToEBITDA: data.EVToEBITDA,
+          SharesOutstanding : data.SharesOutstanding,
+        }
+        res.render("stock", object);
       }
     }
   );
 });
 
-router.get("/:ticker/earnings", (req, res) => {
+router.get("/:ticker/api", (req, res) => {
   const urlTicker = req.params.ticker;
-  var urlEarnings =
-    "https://www.alphavantage.co/query?function=EARNINGS&symbol=" +
+  const urlOverview =
+    "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
     urlTicker +
-    "&apikey=3XWA7YP44XQ5VGAH";
-
+    "&apikey=" +api_key;
   request.get(
     {
-      url: urlEarnings,
+      url: urlOverview,
       json: true,
       headers: { "User-Agent": "request" },
     },
