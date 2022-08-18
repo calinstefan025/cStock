878b6344d24b9488d737ef04db622cb83915dbe4
Avramoniu Calin <clavramoniu@gmail.com>
2022-08-18 16:06:06 +0300

    created main algorithm function

---
+++ public/scripts/gob.js
@@ -0,0 +1,9 @@
+let maxScore = 1000 ;
+let score = 0 ;
+
+const MarketCapitalization = document.getElementById("MarketCapitalization").innerText;
+
+
+function gob() {
+    console.log(MarketCapitalization);
+}
