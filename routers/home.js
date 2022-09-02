d03b69860b8c51f3bb11b52f300c41194268d5d6
Avramoniu Calin <clavramoniu@gmail.com>
2022-09-03 00:05:08 +0300

    about and contact pages

--- routers/home.js
+++ routers/home.js
@@ -5,4 +5,12 @@ router.get("/", function (req, res) {
   res.render("home");
 });
 
+router.get('/about' , function(req ,res) {
+  res.render('about');
+})
+
+router.get('/contact' , function(req ,res) {
+  res.render('contact');
+})
+
 module.exports = router;
